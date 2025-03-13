0d78a2f97c7bd3e845e0455b73a55cc5938c53d9
lakhanihardi <hardi.jgd@gmail.com>
2025-03-13 09:22:09 +0530

    Reverse string of loops

---
+++ Reverse String.py
@@ -0,0 +1,6 @@
+string=input("please enter your own string:")
+string2=('')
+for i in string:
+    string2=i+string2
+print("\nThe original string=",string) 
+print("The Reversed string =",string2)   
